faf651c576cac42030b9895063a0ee5bd3eecf3c
kyerlasswell <kyer@kyer.sh>
2025-02-23 14:32:42 -0600

    fix distance problem

--- misc.c
+++ misc.c
@@ -5,65 +5,65 @@
 
 bool isHolding(OBJECT *container, OBJECT *obj)
 {
-  return obj != NULL && obj->location == container;
+   return obj != NULL && obj->location == container;
 }
 
 OBJECT *getPassage(OBJECT *from, OBJECT *to)
 {
-  if (from != NULL && to != NULL)
-  {
-    OBJECT *obj;
-    for (obj = objs; obj < endOfObjs; obj++)
-    {
-      if (isHolding(from, obj) && obj->destination == to)
+   if (from != NULL && to != NULL)
+   {
+      OBJECT *obj;
+      for (obj = objs; obj < endOfObjs; obj++)
       {
-        return obj;
+         if (isHolding(from, obj) && obj->destination == to)
+         {
+            return obj;
+         }
       }
-    }
-  }
-  return NULL;
+   }
+   return NULL;
 }
 
 DISTANCE getDistance(OBJECT *from, OBJECT *to)
 {
-  return to == NULL                              ? distUnknownObject :
-         to == from                              ? distSelf          :
-         isHolding(from, to)                     ? distHeld          :
-         isHolding(to, from)                     ? distLocation      :
-         isHolding(from->location, to)           ? distHere          :
-         isHolding(from, to->location)           ? distHeldContained :
-         isHolding(from->location, to->location) ? distHereContained :
-         getPassage(from->location, to) != NULL  ? distOverThere     :
-                                                   distNotHere       ;
+   return to == NULL                               ? distUnknownObject :
+          to == from                               ? distSelf          :
+          isHolding(from, to)                      ? distHeld          :
+          isHolding(to, from)                      ? distLocation      :
+          isHolding(from->location, to)            ? distHere          :
+          isHolding(from, to->location)            ? distHeldContained :
+          isHolding(from->location, to->location)  ? distHereContained :
+          getPassage(from->location, to) != NULL   ? distOverThere     :
+                                                     distNotHere       ;
 }
 
 OBJECT *actorHere(void)
 {
-  OBJECT *obj;
-  for (obj = objs; obj < endOfObjs; obj++)
-  {
-    if (isHolding(player->location, obj) && obj == guard)
-    {
-      return obj;
-    }
-  }
-  return NULL;
+   OBJECT *obj;
+   for (obj = objs; obj < endOfObjs; obj++)
+   {
+      if (isHolding(player->location, obj) && obj == guard)
+      {
+         return obj;
+      }
+   }
+   return NULL;
 }
 
 int listObjectsAtLocation(OBJECT *location)
 {
-  int count = 0;
-  OBJECT *obj;
-  for (obj = objs; obj < endOfObjs; obj++)
-  {
-    if (obj != player && isHolding(location, obj))
-    {
-      if (count++ == 0)
+   int count = 0;
+   OBJECT *obj;
+   for (obj = objs; obj < endOfObjs; obj++)
+   {
+      if (obj != player && isHolding(location, obj))
       {
-        printf("You see:\n");
+         if (count++ == 0)
+         {
+            printf("You see:\n");
+         }
+         printf("%s\n", obj->description);
       }
-      printf("%s\n", obj->description);
-    }
-  }
-  return count;
+   }
+   return count;
 }
