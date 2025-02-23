5ef1c6e1605c97264c29618e5b4220b596fdfcef
kyerlasswell <kyer@kyer.sh>
2025-02-23 12:50:33 -0600

    update noun

--- noun.c
+++ noun.c
@@ -30,12 +30,45 @@ OBJECT *getVisible(const char *intention, const char *noun)
   }
   else if (!(obj                     == player           ||
              obj                     == player->location ||
+             obj->location           == player           ||
              obj->location           == player->location ||
              obj->location           == NULL             ||
              obj->location->location == player           ||
              obj->location->location == player->location ))
   {
     printf("You don't see any %s here.\n", noun);
+    obj = NULL;
+  }
+  return obj;
+}
+
+OBJECT *getPosession(OBJECT *from, const char *verb, const char *noun)
+{
+  OBJECT *obj = NULL;
+  if (from == NULL)
+  {
+    printf("I don't understand who you want to %s.\n", verb);
+  }
+  else if ((obj = getObject(noun)) == NULL)
+  {
+    printf("I don't understand what you want to %s.\n", verb);
+  }
+  else if (obj == from)
+  {
+    printf("You should not be doing that to %s.\n", obj->description);
+    obj = NULL;
+  }
+  else if (obj->location != from)
+  {
+    if (from == player)
+    {
+      printf("You are not holding any %s.\n", noun);
+    }
+    else 
+    {
+      printf("There appears to be no %s you can get from %s.\n",
+             noun, from->description);
+    }
     obj = NULL;
   }
   return obj;
--- noun.h
+++ noun.h
@@ -1,1 +1,2 @@
 extern OBJECT *getVisible(const char *intention, const char *noun);
+extern OBJECT *getPosession(OBJECT *from, const char *verb, const char *noun);
